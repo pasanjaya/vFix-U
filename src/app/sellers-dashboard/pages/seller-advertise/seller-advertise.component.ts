4475fdb1e63d01a882a462def72f342698c8edab
pasanjaya <bhadrajeepjaya@gmail.com>
2019-05-25 14:23:56 +0530

    added ui to the seller advert

--- src/app/sellers-dashboard/pages/seller-advertise/seller-advertise.component.ts
+++ src/app/sellers-dashboard/pages/seller-advertise/seller-advertise.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { mimeType } from 'src/app/validators/mime-type.validator';
 
 @Component({
   selector: 'app-seller-advertise',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SellerAdvertiseComponent implements OnInit {
 
+  advImagePreviewPath: string;
+  advertisingForm: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
+    this.advertisingForm = new FormGroup({
+      title: new FormControl(null, Validators.required),
+      description: new FormControl(null, Validators.required),
+      advImage: new FormControl(null, {validators: [Validators.required], asyncValidators: [mimeType]})
+    });
+  }
+
+  onAdvImageUpload(event: Event) {
+    const image = (event.target as HTMLInputElement).files[0];
+    this.advertisingForm.patchValue({advImage: image});
+    this.advertisingForm.get('advImage').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.advImagePreviewPath = reader.result as string;
+    };
+    reader.readAsDataURL(image);
+  }
+
+  onPublish() {
+    if (this.advertisingForm.invalid) {
+      console.log(this.advertisingForm);
+      return;
+    }
   }
 
 }
