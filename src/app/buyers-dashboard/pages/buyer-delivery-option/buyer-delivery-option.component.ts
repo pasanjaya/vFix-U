948ef0548f7f8ef9b5e8c3a4e4d3697ad22720b6
Pasan Jayawickrama <bhadrajeepjaya@gmail.com>
2019-08-12 18:03:32 +0530

    added shipping detailed form

--- src/app/buyers-dashboard/pages/buyer-delivery-option/buyer-delivery-option.component.ts
+++ src/app/buyers-dashboard/pages/buyer-delivery-option/buyer-delivery-option.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-buyer-delivery-option',
@@ -9,7 +10,23 @@ export class BuyerDeliveryOptionComponent implements OnInit {
 
   constructor() { }
 
+  shippingForm: FormGroup;
+
   ngOnInit() {
+    this.shippingForm = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      address: new FormControl(null, Validators.required),
+      city: new FormControl('Colombo', Validators.required),
+      contactNo: new FormControl(null, Validators.required),
+      latitude: new FormControl(7.8731, Validators.required),
+      longitude: new FormControl(80.7718, Validators.required),
+      about: new FormControl(null)
+    });
+  }
+
+
+  onOrder() {
+
   }
 
 }
